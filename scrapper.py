84e307987f3fd90a107254d8934e498c91ce9f7c
Bomi Choi <hibomi97@gmail.com>
2022-04-20 19:35:26 +0900

    예외 처리 코드 추가

--- scrapper.py
+++ scrapper.py
@@ -15,18 +15,21 @@ def get_last_JK_page(url):
     return (total//20) + 1
 
 def extract_JK(html):
-    title = html.find("a", {"class": "title dev_view"}).get_text(strip=True)
-    company = html.find("a", {"class": "name dev_view"}).get_text(strip=True)
-    location = html.find("span", {"class": "loc long"}).get_text(strip=True)
-    time = html.find("span", {"class": "date"}).get_text(strip=True)
-    job_id = html['data-gno']
-    return {
-        'title': title,
-        'company': company,
-        'location': location,
-        'time' : time,
-        'link': f"http://www.jobkorea.co.kr/Recruit/GI_Read/{job_id}"
-    }
+    try:
+        title = html.find("a", {"class": "title dev_view"}).get_text(strip=True)
+        company = html.find("a", {"class": "name dev_view"}).get_text(strip=True)
+        location = html.find("span", {"class": "loc long"}).get_text(strip=True)
+        time = html.find("span", {"class": "date"}).get_text(strip=True)
+        job_id = html['data-gno']
+        return {
+            'title': title,
+            'company': company,
+            'location': location,
+            'time' : time,
+            'link': f"http://www.jobkorea.co.kr/Recruit/GI_Read/{job_id}"
+        }
+    except:
+        return None
 
 def extract_JKs(word):
     url = f"http://www.jobkorea.co.kr/Search/?stext={word}"
@@ -43,7 +46,8 @@ def extract_JKs(word):
             results = soup.find_all("li", {"class": "list-post"})
             for result in results:
                 job = extract_JK(result)
-                jobs.append(job)
+                if job:
+                    jobs.append(job)
     return jobs
 
 
@@ -59,21 +63,24 @@ def get_last_SO_page(url):
         return int(last_page)
 
 def extract_SO(html):
-    parent = html.find("div", {"class": "fl1"})
-    title = parent.find("h2").find("a")["title"]
-    company, location = parent.find("h3").find_all(
-        "span", recursive=False)
-    company = company.get_text(strip=True)
-    location = location.get_text(strip=True)
-    time = parent.find("ul", {"class": "fs-caption"}).find_all("li")[0].get_text(strip=True)
-    job_id = html['data-jobid']
-    return {
-        'title': title,
-        'company': company,
-        'location': location,
-        'time': time,
-        'link': f"https://stackoverflow.com/jobs/{job_id}"
-    }
+    try:
+        parent = html.find("div", {"class": "fl1"})
+        title = parent.find("h2").find("a")["title"]
+        company, location = parent.find("h3").find_all(
+            "span", recursive=False)
+        company = company.get_text(strip=True)
+        location = location.get_text(strip=True)
+        time = parent.find("ul", {"class": "fs-caption"}).find_all("li")[0].get_text(strip=True)
+        job_id = html['data-jobid']
+        return {
+            'title': title,
+            'company': company,
+            'location': location,
+            'time': time,
+            'link': f"https://stackoverflow.com/jobs/{job_id}"
+        }
+    except:
+        return None
 
 def extract_SOs(word):
     url = f"https://stackoverflow.com/jobs?r=true&q={word}"
@@ -89,33 +96,37 @@ def extract_SOs(word):
         results = soup.find_all("div", {"class": "-job"})
         for result in results:
             job = extract_SO(result)
-            jobs.append(job)
+            if job:
+                jobs.append(job)
     return jobs
 
 
 def extract_WWR(html):
-    parent = html.find("a", {"href": re.compile("/remote-jobs")})
-    title = parent.find("span", {"class": "title"}).get_text(strip=True)
-    company = parent.find("span", {"class": "company"}).get_text(strip=True)
-    location = parent.find("span", {"class": "region"})
-    if location is not None:
-        location = location.get_text(strip=True)
-    else:
-        location = "Remote"
-    time = parent.find("span", {"class": "date"})
-    if time is not None:
-        time = time.get_text(strip=True)
-    else:
-        time = "None"
-    
-    job_id = parent["href"]
-    return {
-        'title': title,
-        'company': company,
-        'location': location,
-        'time': time,
-        'link': f"https://weworkremotely.com{job_id}"
-    }
+    try:
+        parent = html.find("a", {"href": re.compile("/remote-jobs")})
+        title = parent.find("span", {"class": "title"}).get_text(strip=True)
+        company = parent.find("span", {"class": "company"}).get_text(strip=True)
+        location = parent.find("span", {"class": "region"})
+        if location is not None:
+            location = location.get_text(strip=True)
+        else:
+            location = "Remote"
+        time = parent.find("span", {"class": "date"})
+        if time is not None:
+            time = time.get_text(strip=True)
+        else:
+            time = "None"
+        
+        job_id = parent["href"]
+        return {
+            'title': title,
+            'company': company,
+            'location': location,
+            'time': time,
+            'link': f"https://weworkremotely.com{job_id}"
+        }
+    except:
+        return None
 
 def extract_WWRs(word):
     url = f"https://weworkremotely.com/remote-jobs/search?term={word}"
@@ -126,7 +137,8 @@ def extract_WWRs(word):
     results = soup.find_all("li", {"class": "feature"})
     for result in results:
         job = extract_WWR(result)
-        jobs.append(job)
+        if job:
+            jobs.append(job)
     return jobs
 
 
